718ea15386ab5f7df559cdd3735aaff56bd7a39a
Alfonso Bravo <alfonso.bravo.molina@gmail.com>
2025-02-06 06:55:28 +0100

    Implement contact form functionality with form submission and error handling

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TopmenuComponent } from '../topmenu/topmenu.component';
 import { FooterComponent } from '../footer/footer.component';
+import { FormsModule } from '@angular/forms';
+import { SingleService } from '../../services/single.service';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [TopmenuComponent, FooterComponent],
+  imports: [TopmenuComponent, FooterComponent, FormsModule],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.css',
 })
-export class ContactUsComponent {}
+export class ContactUsComponent {
+  singleService = inject(SingleService);
+
+  name: string = '';
+  email: string = '';
+  message: string = '';
+  jobtitle: string = '';
+  phone: string = '';
+
+  // Añadimos variables para controlar el estado
+  isSubmitted: boolean = false;
+  hasError: boolean = false;
+  errorMessage: string = '';
+
+  onSubmit() {
+    this.singleService
+      .sendFormData({
+        name: this.name,
+        email: this.email,
+        message: `${this.name} - Phone: ${this.phone} - Job title:${this.jobtitle} - has sent the following message from the contact form: \n \n ${this.message}`,
+      })
+      .then(() => {
+        this.isSubmitted = true;
+        this.resetForm();
+      })
+      .catch((error: Error) => {
+        this.hasError = true;
+        this.errorMessage =
+          'Ha ocurrido un error al enviar el mensaje. Por favor, inténtelo de nuevo.';
+        console.error('Error al enviar el formulario:', error);
+      });
+  }
+
+  resetForm() {
+    this.name = '';
+    this.email = '';
+    this.message = '';
+    this.jobtitle = '';
+    this.phone = '';
+  }
+}
--- src/app/services/single.service.ts
+++ src/app/services/single.service.ts
@@ -1,10 +1,23 @@
-import { Injectable, signal, WritableSignal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal, WritableSignal } from '@angular/core';
+
+import { environment } from '../../environments/environment';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SingleService {
   signalVisorVisibility: WritableSignal<boolean> = signal(false);
+  http = inject(HttpClient);
+
+  URL = environment.URL;
 
   constructor() {}
+
+  sendFormData({ name, email, message }: any) {
+    return firstValueFrom(
+      this.http.post(`${this.URL}/api/mail`, { name, email, message })
+    );
+  }
 }
